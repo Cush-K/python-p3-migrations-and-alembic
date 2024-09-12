c3f3541da0b9aaa5ccfc5c0e5b5395ca9aa6a7cb
Kalvine <kkalvine4@gmail.com>
2024-09-12 23:42:30 +0300

    Setting up Alembic and migrations

---
+++ app/models.py
@@ -0,0 +1,40 @@
+from datetime import datetime 
+
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import (create_engine, PrimaryKeyConstraint, UniqueConstraint, CheckConstraint,
+                        Column, String, Integer, DateTime, Index)
+
+
+engine = create_engine('sqlite:///migrations_test.db')
+
+Base = declarative_base()
+
+class Student(Base):
+    __tablename__ = 'students'
+    __table_args__= (
+        PrimaryKeyConstraint(
+            'id',
+            name= 'id_pk'
+        ),
+        UniqueConstraint(
+            'email',
+            name = "unique_email"
+        ),
+        CheckConstraint(
+            'grade BETWEEN 1 AND 12',
+            name='grade_between_1_and_12'
+        )
+    )
+    
+        
+    id = Column(Integer())
+    name = Column(String(), index = True)
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default = datetime.now())
+    
+    def __repr__(self):
+        return f"Student {self.id}: " \
+            + f"{self.name}, " \
+            + f"Grade {self.grade}"
